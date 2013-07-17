c1db7778d4cee23dc1b1238bc544c06b8ee39d9c
Robert Blumen <robert@symphonycommerce.com>
2013-07-17 12:34:25 -0700

    initial load

---
+++ src/main/java/com/cache/App.java
@@ -0,0 +1,67 @@
+package com.cache;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+                
+        HelloWorld hw1 = (HelloWorld) context.getBean("hello");
+        HelloWorld hw2 = (HelloWorld) context.getBean("hey");
+
+        App app = new App();
+        app.ruly(hw1);
+        app.longDist(hw2);
+       //ruly(hw);
+    }
+
+    void ruly(HelloWorld hw) {
+    	System.out.println("RULY");
+        String v1= hw.getRulyComplicated();
+        System.out.println("first v of ruly v1=" + v1);
+                
+        String v2= hw.getRulyComplicated();
+        System.out.println("second v of ruly, should be same as first v2=" + v2); 
+
+        hw.hello();
+
+        String v3= hw.getRulyComplicated();
+        System.out.println("should have new value of ruly v3=" + v3); 
+
+        String v4= hw.getRulyComplicated();
+        System.out.println("should repeat new value of ruly v4=" + v4); 
+    }
+
+    void longDist(HelloWorld hw) {
+    	System.out.println("LONG DIST");
+        String v1= hw.getLongDistance();
+        System.out.println("first v of ld y1=" + v1);
+                
+        String v2= hw.getLongDistance();
+        System.out.println("second v of ld y2=" + v2); 
+
+        //hw.hello();
+
+        String v3= hw.getLongDistance();
+        System.out.println("third v of ld y3=" + v3); 
+
+        String v4= hw.getLongDistance();
+        System.out.println("fourth v of ld y4=" + v4); 
+
+        hw.bye();
+
+        v3= hw.getLongDistance();
+        System.out.println("should have new ld y5=" + v3); 
+
+        v4= hw.getLongDistance();
+        System.out.println("should repeat ld y5 y6=" + v4); 
+        
+    }
+
+}
---
+++ src/main/java/com/cache/HelloWorld.java
@@ -0,0 +1,52 @@
+package com.cache;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+
+/**
+ * Spring bean
+ * 
+ */
+public class HelloWorld {
+    private String name;
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+    @Cacheable(value="stuff", key="#root.target.hashCode()")
+    public String getRulyComplicated() {
+        String value = new Double(Math.random() * 1000.0).toString(); 
+        logr("calculating ruly complicated thang=" + value);
+        return value ;
+    }
+        
+    @Cacheable(value="other-stuff", key="#root.target.hashCode()")
+    public String getLongDistance() {
+        String value = new Double(Math.random() * 1000.0).toString(); 
+        logr("calculating long distance=" + value);
+        return value ;
+    }
+
+    @CacheEvict(value="stuff", allEntries = true)
+    public void hello() {
+        logr("evicted stuff");
+    }
+        
+    @CacheEvict(value="other-stuff", allEntries = true)
+    public void bye() {
+        logr("evicting other-stuff");
+    }
+
+    private void logr(String s) {
+        System.out.println("[" + this.name + "]: " + s);
+    }
+
+    public int hashCode() {
+        int hc = name.hashCode();
+        //logr("hashing=" + hc);
+        return hc; 
+    }
+
+
+}
